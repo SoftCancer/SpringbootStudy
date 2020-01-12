c61f30a05b5e8f5752fb3f8b260e385f700a8162
SoftCancer <ygxzzu@163.com>
2020-01-13 01:34:10 +0800

    在使用自动生成代码器generator 时，生成代码后，删除生成的接口及mapper.xml 中对应的接口SQL，即可启动。

--- boot_config_10_multi_datasource/src/main/java/com/dongl/boot_config_10_multi_datasource/BootMultiSourceApplication.java
+++ boot_config_10_multi_datasource/src/main/java/com/dongl/boot_config_10_multi_datasource/BootMultiSourceApplication.java
@@ -1,7 +1,9 @@
 package com.dongl.boot_config_10_multi_datasource;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import tk.mybatis.spring.annotation.MapperScan;
 
 //@MapperScan("com.dongl.boot_config_10_multi_datasource.dao")
--- boot_config_10_multi_datasource/src/main/java/com/dongl/boot_config_10_multi_datasource/dao/master/UserMapper.java
+++ boot_config_10_multi_datasource/src/main/java/com/dongl/boot_config_10_multi_datasource/dao/master/UserMapper.java
@@ -4,11 +4,8 @@ import com.dongl.boot_config_10_multi_datasource.entity.User;
 import com.dongl.boot_config_10_multi_datasource.entity.UserExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
-@Repository
 public interface UserMapper extends Mapper<User> {
-
     User getUserByName(String name);
 }
